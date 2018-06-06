5d569126346c00f36f495b5947b2a4b3dca4fddb
Ajitesh Nair <ajiteshnair10@gmail.com>
2018-06-06 13:55:33 +0530

    ready to submit

--- Assignment_2.py
+++ Assignment_2.py
@@ -1,1 +1,46 @@
-print(" Thank you for reading this commit :) :P ")
+#Question 1
+print("\nQ1\n")
+print(" Anything you want. \n")
+
+
+#Question 2
+#include spaces for better readability/cleaner code
+print("\nQ2\n")
+print(" Acad" + "View")
+
+
+#Question 3
+print("\nQ3\n")
+x = input(" Enter variable x : ")
+y = input(" Enter variable y : ")
+z = input(" Enter variable z : ")
+print(" Value of x is",x)
+print(" Value of y is",y)
+print(" Value of z is",z,"\n")
+
+
+#Question 4
+#We can use escape character('\') to output thinds like / ' "
+print("\nQ4\n")
+print("Let's get started \n")
+print('Let\'s get started \n')
+
+
+#Question 5
+"""
+Placeholder is a pre-formatted container into which content can be placed.
+Examples : %s %d
+"""
+print("\nQ5\n")
+s = "Acadview"
+course = "Python"
+fees = 5000
+print(" %s \n %s \n %d \n"%(s,course,fees))
+
+
+#Question 6
+print("\nQ6\n")
+name = "Tony Stark"
+salary = 1000000
+print(('%s %d')%(name,salary))
+
